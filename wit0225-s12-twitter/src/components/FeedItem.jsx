7332edd54d33e82fc764fd4d2d62dc48d049951f
Emre Şahiner <emre@workintech.com.tr>
2025-05-02 10:17:34 +0300

    adds time to feed item

--- wit0225-s12-twitter/src/components/FeedItem.jsx
+++ wit0225-s12-twitter/src/components/FeedItem.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { formatDistanceToNow } from "date-fns";
+import { tr } from "date-fns/locale";
+
 import Analytics from "./Analytics";
 
 function FeedItem({ tweet }) {
+  console.log(tweet);
+  const time = formatDistanceToNow(tweet.createdAt, {
+    addSuffix: true,
+    locale: tr,
+  });
   return (
     <div className="w-full p-8 border-b border-b-slate-100 flex gap-2">
       <div className="w-15 p-1">
@@ -13,7 +21,7 @@ function FeedItem({ tweet }) {
         <p className="fonr-bold">
           {tweet.userName}{" "}
           <span className="font-normal text-slate-400 text-md">
-            @{tweet.userName} - 25min ago
+            @{tweet.userName} - {time}
           </span>{" "}
         </p>
         <p>{tweet.message}</p>
