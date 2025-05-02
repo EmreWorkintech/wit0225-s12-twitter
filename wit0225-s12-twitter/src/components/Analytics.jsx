133571420a9ff1f28c1fd963d0711a6a60177c38
Emre Şahiner <emre@workintech.com.tr>
2025-05-02 09:54:07 +0300

    adds analytic content

--- wit0225-s12-twitter/src/components/Analytics.jsx
+++ wit0225-s12-twitter/src/components/Analytics.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowsRotate,
+  faArrowUpFromBracket,
+  faChartSimple,
+} from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faMessage } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Analytics() {
+function Analytics({
+  analytics: { like_count, comment_count, retweet_count },
+}) {
   return (
     <div className="flex justify-between">
-      <div>
-        <FontAwesomeIcon icon={faEllipsis} /> 1
+      <div className="flex-1">
+        <FontAwesomeIcon icon={faMessage} /> {comment_count}
       </div>
-      <div>
-        <FontAwesomeIcon icon={faEllipsis} /> 1
+      <div className="flex-1">
+        <FontAwesomeIcon icon={faArrowsRotate} /> {retweet_count}
       </div>
-      <div>
-        <FontAwesomeIcon icon={faEllipsis} /> 1
+      <div className="flex-1">
+        <FontAwesomeIcon icon={faHeart} /> {like_count}
       </div>
-      <div>
-        <FontAwesomeIcon icon={faEllipsis} /> 1
+      <div className="flex-1">
+        <FontAwesomeIcon icon={faChartSimple} />
       </div>
-      <div>
-        <FontAwesomeIcon icon={faEllipsis} /> 1
+      <div className="flex-1">
+        <FontAwesomeIcon icon={faArrowUpFromBracket} />
       </div>
-      <div></div>
+      <div className="flex-1"></div>
     </div>
   );
 }
